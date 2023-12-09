66a0c4315a1de2acb48b6c8f8d3e3b8c8364bc36
Reynaldy Marchell <reynalldymarchell03@gmail.com>
2023-12-09 21:17:46 +0700

    Update all cards

--- src/Components/LandingpageComponent/Cards.jsx
+++ src/Components/LandingpageComponent/Cards.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import Aldy from "../../assets/68384.jpg"
-import Kenny from "../../assets/S__30703618_0.jpg"
+import Aldy from "../../assets/aldy.jpg"
+import Kenny from "../../assets/Kenny.jpg"
+import Ziddan from "../../assets/imagezz.png"
+import Nicho from "../../assets/S__82698283.jpg"
+import Bagier from "../../assets/Pasfotob.jpg"
 
 function Cards() {
     return (
@@ -19,39 +22,39 @@ function Cards() {
                     </div>
                 </div>
                 <div className='w-full shadow-xl flex flex-col p-4 rounded-lg hover:scale-105 duration-300 mt-10 bg-[#EAECEE ]'>
-                    <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Aldy} alt="" />
-                    <h2 className='text-2xl font-bold text-center py-8 text-gray-600'>2602119915</h2>
-                    <p className='text-center text-2xl font-bold'>MUHAMMAD ZIDDAN AZZAKY</p>
-                    <div className='text-center font-medium'>
-                        <p className='py-2 border-b mx-8 mt-8 text-xl'>Web Developer</p>
-                        <p className='py-2 border-b mx-8 '><i>"Ngoding itu berat aku ga bakal kuat biar Chat gpt aja"</i></p>
-                    </div>
-                </div>
-                <div className='w-full shadow-xl flex flex-col p-4 rounded-lg hover:scale-105 duration-300 mt-10 bg-[#EAECEE ]'>
                     <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Kenny} alt="" />
                     <h2 className='text-2xl font-bold text-center py-8 text-gray-600'>2602115015</h2>
                     <p className='text-center text-2xl font-bold'>AARON KENNY RIJADI</p>
                     <div className='text-center font-medium'>
-                        <p className='py-2 border-b mx-8 mt-8 text-xl'>Course Tutor</p>
+                        <p className='py-2 border-b mx-8 mt-8 text-xl'>Course Tutor & Algorithm Designer</p>
                         <p className='py-2 border-b mx-8 '><i>"Once you change the coin, you need to recount the coin change"</i></p>
                     </div>
                 </div>
                 <div className='w-full shadow-xl flex flex-col p-4 rounded-lg hover:scale-105 duration-300 mt-10 bg-[#EAECEE ]'>
-                    <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Aldy} alt="" />
+                    <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Ziddan} alt="" />
+                    <h2 className='text-2xl font-bold text-center py-8 text-gray-600'>2602119915</h2>
+                    <p className='text-center text-2xl font-bold'>MUHAMMAD ZIDDAN AZZAKY</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8 text-xl'>Web Developer</p>
+                        <p className='py-2 border-b mx-8 '><i>"yes I'm  a Jawir: jawa with insane rizz"</i></p>
+                    </div>
+                </div>
+                <div className='w-full shadow-xl flex flex-col p-4 rounded-lg hover:scale-105 duration-300 mt-10 bg-[#EAECEE ]'>
+                    <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Nicho} alt="" />
                     <h2 className='text-2xl font-bold text-center py-8 text-gray-600'>2602138113</h2>
                     <p className='text-center text-2xl font-bold'>NICHOLAS CHRISTIAN</p>
                     <div className='text-center font-medium'>
                         <p className='py-2 border-b mx-8 mt-8 text-xl'>UI/UX Designer</p>
-                        <p className='py-2 border-b mx-8 '><i>"Ngoding itu berat aku ga bakal kuat biar Chat gpt aja"</i></p>
+                        <p className='py-2 border-b mx-8 '><i>"There is no Absolute Truth"</i></p>
                     </div>
                 </div>
                 <div className='w-full shadow-xl flex flex-col p-4 rounded-lg hover:scale-105 duration-300 mt-10 bg-[#EAECEE ]'>
-                    <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Aldy} alt="" />
+                    <img className='w-40 mx-auto mt-[-3rem] bg-white rounded-3xl' src={Bagier} alt="" />
                     <h2 className='text-2xl font-bold text-center py-8 text-gray-600'>2602134531</h2>
                     <p className='text-center text-2xl font-bold'>MUHAMMAD BAGIER ASSEGGAF</p>
                     <div className='text-center font-medium'>
                         <p className='py-2 border-b mx-8 mt-8 text-xl'>UI/UX Designer</p>
-                        <p className='py-2 border-b mx-8 '><i>"Ngoding itu berat aku ga bakal kuat biar Chat gpt aja"</i></p>
+                        <p className='py-2 border-b mx-8 '><i>" 1 + 1 = 2 "</i></p>
                     </div>
                 </div>
             </div>
